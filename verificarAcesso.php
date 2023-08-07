7e203e3b0e2cf8ac3d3511868b4f462d5c95a4e3
Diogo Alberto <kirchoffalberto@gmail.com>
2023-08-07 16:15:43 -0300

    Função retorna uma string indicando se a pessoa pode ou não ter acesso a um determinado local.

---
+++ verificarAcesso.php
@@ -0,0 +1,13 @@
+<?php
+    function verificarAcesso($a, $b) {
+        if ($a >= 18 && $b == true)
+            echo "Maior de 18 anos";
+        else if ($a < 18)
+            echo "Acesso negado. Idade mínima requerida: 18 anos.";
+        else if ($a >= 18 && $b == false)
+            echo "Acesso negado. Autorização necessária.";
+    }
+
+    verificarAcesso(18, true);
+
+?>
